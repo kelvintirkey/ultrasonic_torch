e56827560d4ab8ef539d30df0eecd831b6270151
kelvintirkey <kelvin.tirkey@gmail.com>
2022-07-30 00:21:54 +0530

    First Commit

---
+++ US_Torch.ino
@@ -0,0 +1,50 @@
+#define trigPin 3
+#define echoPin 2
+#define buzzer 6
+#define motorPin 7
+
+
+long duration;
+int distance;
+int safetyDistance;
+
+
+void setup() {
+pinMode(trigPin, OUTPUT); 
+pinMode(echoPin, INPUT); 
+pinMode(buzzer, OUTPUT);
+pinMode(motorPin, OUTPUT);
+Serial.begin(9600); 
+}
+
+
+void loop() {
+
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+
+
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+
+duration = pulseIn(echoPin, HIGH);
+
+
+distance= duration*0.034/2;
+
+safetyDistance = distance;
+if (safetyDistance <= 30){
+  digitalWrite(buzzer, HIGH);
+  digitalWrite(motorPin, HIGH);
+}
+else{
+  digitalWrite(buzzer, LOW);
+  digitalWrite(motorPin, LOW);
+}
+
+
+Serial.print("Distance: ");
+Serial.println(distance);
+}
