8049afa89b08b7acd9d0810299d4ef51274e43d1
kelvintirkey <kelvin.tirkey@gmail.com>
2022-07-30 13:32:45 +0530

    Updated (new function)

--- US_Torch.ino
+++ US_Torch.ino
@@ -1,11 +1,14 @@
 #define trigPin 3
 #define echoPin 2
 #define buzzer 6
-#define motorPin 7
+#define motorPin 13
+#define buttonPin 7
 
-
+int count=4;
+int buttonState = 0; 
 long duration;
 int distance;
+int safeDistance;
 
 
 void setup() {
@@ -13,11 +16,35 @@ pinMode(trigPin, OUTPUT);
 pinMode(echoPin, INPUT); 
 pinMode(buzzer, OUTPUT);
 pinMode(motorPin, OUTPUT);
+pinMode(buttonPin, INPUT); 
 Serial.begin(9600); 
 }
 
 
 void loop() {
+
+buttonState = digitalRead(buttonPin);
+    
+if (buttonState == HIGH) {
+  if (count<9){
+    count ++;
+    for (int i=0; i<(count-3); i++){
+    digitalWrite(buzzer, HIGH);
+    delay(500);
+    digitalWrite(buzzer, LOW);
+    delay(500);
+    }
+  }
+  else{
+    count=4;
+    for (int i=0; i<(count-3); i++){
+      digitalWrite(buzzer, HIGH);
+      delay(500);
+      digitalWrite(buzzer, LOW);
+      delay(500);
+    }
+  }
+}  
 
 digitalWrite(trigPin, LOW);
 delayMicroseconds(2);
@@ -32,8 +59,10 @@ duration = pulseIn(echoPin, HIGH);
 
 
 distance= duration*0.034/2;
+safeDistance = count*10;
 
-if (distance <= 30){
+
+if (distance <= safeDistance){
   digitalWrite(buzzer, HIGH);
   digitalWrite(motorPin, HIGH);
 }
